2a4c54c9efbda9eae315f2e8e0ff715e8a8e158b
CHO1116 <58311050+CHO1116@users.noreply.github.com>
2022-01-24 18:26:16 +0900

    ex05 will be retry

--- C03/ex05/ft_strlcat.c
+++ C03/ex05/ft_strlcat.c
@@ -6,29 +6,33 @@
 /*   By: youngjch <youngjch@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/19 20:42:22 by youngjch          #+#    #+#             */
-/*   Updated: 2022/01/20 11:30:21 by youngjch         ###   ########.fr       */
+/*   Updated: 2022/01/24 18:20:51 by youngjch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
-	unsigned int	dest_i;
-	unsigned int	src_i;
+	unsigned int	dest_len;
 	unsigned int	src_len;
+	unsigned int	i;
 
-	dest_i = 0;
-	src_i = 0;
+	dest_len = 0;
 	src_len = 0;
-	while (dest[dest_i] != '\0' && dest_i < size)
-		dest_i++;
+	i = 0;
+	while (dest[dest_len] != '\0')
+		dest_len++;
 	while (src[src_len] != '\0')
 		src_len++;
-	while (src[src_i] != '\0' && (dest_i + src_i) < size - 1)
+	if (size - 1 < dest_len)
+		return (size + src_len);
+	if (size - 1 > dest_len)
 	{
-		dest[dest_i + src_i] = src[src_i];
-		src_i++;
+		while (src[i] != '\0' && dest_len + i < size - 1)
+		{
+			dest[dest_len + i] = src[i];
+			i++;
+		}
 	}
-	if (dest_i + src_i < size)
-		dest[dest_i + src_i] = '\0';
-	return (dest_i + src_len);
+	dest[dest_len + i] = '\0';
+	return (dest_len + src_len);
 }
