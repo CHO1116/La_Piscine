cb42ecd6994f4b7183cdf8e75dfcf9b943cc1f61
CHO1116 <58311050+CHO1116@users.noreply.github.com>
2022-01-27 03:08:53 +0900

    ex08 was modified

--- C00/ex08/ft_print_combn.c
+++ C00/ex08/ft_print_combn.c
@@ -1,48 +1,74 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_print_combn.c                                   :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: youngjch <youngjch@student.42seoul.>       +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/01/11 16:02:30 by youngjch          #+#    #+#             */
-/*   Updated: 2022/01/13 16:06:22 by youngjch         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <unistd.h>
-void	ft_putchar(char c);
 
-void	ft_print_combn(int n)
+void	ft_putchar(char ch)
 {
-	int		arr[10];
-	int		init;
-	int		index;
+	write(1, &ch, 1);
+}
 
-	init = 0;
-	index = 0;
-	while (init < n)
+void	ft_print_numset(int *arr, int n)
+{
+	int	i;
+
+	i = 0;
+	while (i < n)
 	{
-		arr[init] = 0;
-		init++;
-	}
-	while (index < n - 1)
-	{
-		if (index != 1)
-		{
-			ft_print_combn(n - 1);
-		}
-		while (arr[n - index - 1] <= 9)
-		{
-			ft_putchar(48 + arr[n - index - 1] % 10);
-			arr[n - index - 1]++;
-			write(1, ", ", 1);
-		}
-		index++;
+		ft_putchar(arr[i] + '0');
+		i++;
 	}
 }
 
-void	ft_putchar(char c)
+void	ft_nums_up(int *arr, int n, int pre_idx)
 {
-	write(1, &c, 1);
+	arr[pre_idx]++;
+	while (pre_idx < n)
+	{
+		arr[pre_idx + 1] = arr[pre_idx] + 1;
+		pre_idx++;
+	}
+}
+
+int	ft_get_index(int *arr, int n)
+{
+	int i;
+	int max;
+
+	i = 0;
+	max = 10 - n;
+	while (i < n)
+	{
+		if (arr[i] == max)
+			return (i - 1);
+		else if (i == (n - 1) && arr[i] < max)
+			return (i);
+		i++;
+		max++;
+	}
+	return (-1);
+}
+
+void	ft_print_combn(int n)
+{
+	int	arr[n + 1];
+	int	i;
+	int	pre_idx;
+
+	if (n >= 10 || n <= 0)
+		return ;
+	else
+	{
+		i = 0;
+		while (i < n)
+		{
+			arr[i] = i;
+			i++;
+		}
+		while (ft_get_index(arr, n) != -1)
+		{
+			ft_print_numset(arr, n);
+			pre_idx = ft_get_index(arr, n);
+			ft_nums_up(arr, n, pre_idx);
+			write(1, ", ", 2);
+		}
+		ft_print_numset(arr, n);
+	}
 }
